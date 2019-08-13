dc2bfd99d36e7898e171dd5f1a138d8f0a20dfbf
callingYou <2544691871@qq.com>
2019-08-13 20:42:36 +0800

    第一次提交

---
+++ src/main/java/com/znsd/git/entities/Student.java
@@ -0,0 +1,25 @@
+package com.znsd.git.entities;
+
+public class Student {
+	private Integer no;
+	private String name;
+	private String email;
+	public Integer getNo() {
+		return no;
+	}
+	public void setNo(Integer no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
